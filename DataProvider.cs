103dfd3e23a0d4502c7ddd947a70766aab67832e
onedee7012 <filmholic7012@gmail.com>
2025-12-03 16:24:50 +0700

    Update DataProvider.cs

--- DataProvider.cs
+++ DataProvider.cs
@@ -19,19 +19,14 @@ namespace Cafeteria.DAL
         }
 
         private DataProvider() { }
-
-        private string connectionSTR = @"Data Source=.\SQLEXPRESS;Initial Catalog=Cafe;Integrated Security=True";
+        private string connectionSTR = @"Data Source=.\SQLEXPRESS;Initial Catalog=CafeManagement;Integrated Security=True";
         public DataTable ExecuteQuery(string query, object[] parameter = null)
         {
             DataTable data = new DataTable();
-
             using (SqlConnection connection = new SqlConnection(connectionSTR))
             {
-
                 connection.Open();
-
                 SqlCommand command = new SqlCommand(query, connection);
-
                 if (parameter != null)
                 {
                     string[] listPara = query.Split(' ');
@@ -44,30 +39,21 @@ namespace Cafeteria.DAL
                             i++;
                         }
                     }
-
                 }
-
                 SqlDataAdapter adapter = new SqlDataAdapter(command);
-
                 adapter.Fill(data);
-
                 connection.Close();
             }
-
             return data;
         }
 
         public int ExecuteNonQuery(string query, object[] parameter = null)
         {
             int data = 0;
-
             using (SqlConnection connection = new SqlConnection(connectionSTR))
             {
-
                 connection.Open();
-
                 SqlCommand command = new SqlCommand(query, connection);
-
                 if (parameter != null)
                 {
                     string[] listPara = query.Split(' ');
@@ -80,28 +66,19 @@ namespace Cafeteria.DAL
                             i++;
                         }
                     }
-
                 }
-
                 data = command.ExecuteNonQuery();
-
                 connection.Close();
             }
-
             return data;
         }
-
         public object ExecuteScalar(string query, object[] parameter = null)
         {
             object data = 0;
-
             using (SqlConnection connection = new SqlConnection(connectionSTR))
             {
-
                 connection.Open();
-
                 SqlCommand command = new SqlCommand(query, connection);
-
                 if (parameter != null)
                 {
                     string[] listPara = query.Split(' ');
@@ -114,15 +91,12 @@ namespace Cafeteria.DAL
                             i++;
                         }
                     }
-
                 }
-
                 data = command.ExecuteScalar();
-
                 connection.Close();
             }
-
             return data;
         }
     }
 }
+
